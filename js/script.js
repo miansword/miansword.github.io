bc5bbd1bf5b11c1bab880d2e5190cda9ba331f42
miansword <2558974811@qq.com>
2015-06-29 16:38:58 +0800

    Site updated: 2015-06-29 16:38:58

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(function(){
     $(document).pjax('a[data-pjax]','#wrap',{fragment:'#wrap',timeout:10000}).on('pjax:send',function(){
      	return $('#wrap').fadeTo(0,0);
      }).on('pjax:complete',function(){
-     	return $('#wrap').fadeTo("fast",1);
      	pajx_loadDuodsuo();
+     	return $('#wrap').fadeTo("fast",1);
      });
 })
 
