7966a5a089073bde6b62f15f96bfe499c48ff46a
miansword <2558974811@qq.com>
2015-06-29 16:48:04 +0800

    Site updated: 2015-06-29 16:48:04

--- js/script.js
+++ js/script.js
@@ -2,15 +2,14 @@ $(function(){
     $(document).pjax('a[data-pjax]','#wrap',{fragment:'#wrap',timeout:10000}).on('pjax:send',function(){
      	return $('#wrap').fadeTo(0,0);
      }).on('pjax:complete',function(){
-     	pajx_loadDuodsuo();
+     	_loadDuodsuo();
      	return $('#wrap').fadeTo("fast",1);
      });
-})
 
-/**
+     /**
  * pjax后需要回调函数.加载多说
  */
-function pajx_loadDuodsuo(){
+function _loadDuodsuo(){
 	var dus=$(".ds-thread");
 	if($(dus).length==1){
 		var el = document.createElement('div');
@@ -20,3 +19,4 @@ function pajx_loadDuodsuo(){
 		$(dus).html(el);
 	}
 }
+})
