0c4e8dee9a600b8fcc817e4b9ec17397016f56dd
miansword <2558974811@qq.com>
2015-06-29 16:33:21 +0800

    Site updated: 2015-06-29 16:33:21

--- js/script.js
+++ js/script.js
@@ -3,5 +3,20 @@ $(function(){
      	return $('#wrap').fadeTo(0,0);
      }).on('pjax:complete',function(){
      	return $('#wrap').fadeTo("fast",1);
+     	pajx_loadDuodsuo();
      });
 })
+
+/**
+ * pjax后需要回调函数.加载多说
+ */
+function pajx_loadDuodsuo(){
+	var dus=$(".ds-thread");
+	if($(dus).length==1){
+		var el = document.createElement('div');
+		el.setAttribute('data-thread-key',$(dus).attr("data-thread-key"));//必选参数
+		el.setAttribute('data-url',$(dus).attr("data-url"));
+		DUOSHUO.EmbedThread(el);
+		$(dus).html(el);
+	}
+}
